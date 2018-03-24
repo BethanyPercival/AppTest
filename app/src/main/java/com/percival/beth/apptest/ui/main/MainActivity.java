7bede92700928816dc76bea3462977e0cf4ac7ba
BethanyPercival <bpercival1802@gmail.com>
2018-03-24 17:58:00 +0000

    0TB2 - Added the tab layout with very basic fragments. The tabs are working.

---
+++ app/src/main/java/com/percival/beth/apptest/ui/main/MainActivity.java
@@ -0,0 +1,34 @@
+package com.percival.beth.apptest.ui.main;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.percival.beth.apptest.R;
+
+import butterknife.BindView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        MainFragmentPagerAdapter adapter = new MainFragmentPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(adapter);
+        SmartTabLayout tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.setViewPager(viewPager);
+    }
+}
